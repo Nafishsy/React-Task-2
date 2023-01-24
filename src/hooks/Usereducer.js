1caa97fb9ee5eafb1f5ab1cdf4d807c08cdf13cc
Nafiz Ahmed <nafizahmed273273@gmail.com>
2023-01-24 14:34:11 +0600

    useRef useReducer

--- src/hooks/Usereducer.js
+++ src/hooks/Usereducer.js
@@ -1,12 +1,49 @@
+import { useReducer, useState } from "react"
 import AllHooks from "./AllHooks"
 
+
+const ACTIONS = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement',
+}
+
+const reducer = (state,action) => {
+    switch(action.type){
+        case ACTIONS.INCREMENT:
+            return {count: state.count + 1 }
+        case ACTIONS.DECREMENT:
+            return {count: state.count - 1 }
+        default:
+            return state
+    }
+}
+
 const UseReducer =()=>{
+
+    const [state,dispatch] = useReducer(reducer, {count: 0})
+
+    const inc = () =>{
+        //setCount(count +1)
+        dispatch({ type: ACTIONS.INCREMENT})
+    }
+
     
+    const dec = () =>{
+        // setCount(count-1)
+        dispatch({ type: ACTIONS.DECREMENT})
+
+    }
+
+
     return(
         <div>
             <AllHooks/>
             <h1>UseReducer</h1>
 
+
+            <button onClick={dec}>-</button>
+            <span>{state.count}</span>
+            <button onClick={inc}>+</button>
 
         </div>
     )
--- src/hooks/Useref.js
+++ src/hooks/Useref.js
@@ -1,12 +1,25 @@
+import { useRef, useState } from "react"
 import AllHooks from "./AllHooks"
 
 const UseRef =()=>{
     
+    const [name,setName] = useState('')
+    const inputRef = useRef()
+
+    const focus = () =>{
+        inputRef.current.focus()
+    }
     return(
         <div>
             <AllHooks/>
             <h1>UseRef</h1>
 
+            <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}
+            />
+            <br></br>
+
+            <span>My name is {name}</span>
+            <button onClick={focus}>Focus</button>
             
         </div>
     )
